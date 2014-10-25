f3f75e4e16eaf642854d3d93f93b7cad1b5715f1
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2014-10-26 01:16:19 +0700

    Add Listen on Port using JAVA

---
+++ Util/ListenOnPort/ListenOnPort.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ListenOnPort{
+	public static void main( String [] args ){
+		int port = Integer.parseInt( args[0] );
+		try{
+			ServerSocket serverSocket = new ServerSocket( port );
+			new Thread("Device Listener"){
+				public void run() {
+					try {
+						System.out.println("Start Listening on Port "+port);
+						Socket socket;
+						while ((socket = serverSocket.accept()) != null) {
+							System.out.println("| Incoming : "+ socket.toString());
+						}
+					} catch (Exception e) {				
+					}
+				};
+			}.start();
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
